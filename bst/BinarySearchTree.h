96e519e7e74e4ffa752e694a23045f9f66edb083
franq200 <kuj-fran@wp.pl>
2023-03-13 19:35:00 +0100

    added BinarySearchTree class and Insert()

---
+++ bst/BinarySearchTree.h
@@ -0,0 +1,74 @@
+#pragma once
+
+template <typename T>
+struct Node
+{
+	T value;
+	Node<T>* left = nullptr;
+	Node<T>* right = nullptr;
+};
+
+template <typename T>
+class BinarySearchTree
+{
+public:
+	void Insert(T value);
+	const T& Search(T value);
+	void Remove(T value);
+	size_t GetSize() const;
+private:
+	size_t m_size;
+	Node<T>* m_root = nullptr;
+};
+
+template<typename T>
+inline void BinarySearchTree<T>::Insert(T value)
+{
+	if (m_root == nullptr)
+	{
+		m_root = new Node<T>{value};
+	}
+	else
+	{
+		Node<T>* currentNode = m_root;
+		Node<T>* beforeCurrentNode = nullptr;
+		while (currentNode != nullptr)
+		{
+			beforeCurrentNode = currentNode;
+			if (value < currentNode->value)
+			{
+				currentNode = currentNode->left;
+			}
+			else if (value > currentNode->value)
+			{
+				currentNode = currentNode->right;
+			}
+		}
+
+		if (value < beforeCurrentNode->value)
+		{
+			beforeCurrentNode->left = new Node<T>{ value };
+		}
+		else if (value > beforeCurrentNode->value)
+		{
+			beforeCurrentNode->right = new Node<T>{ value };
+		}
+	}
+}
+
+template<typename T>
+inline const T& BinarySearchTree<T>::Search(T value)
+{
+	
+}
+
+template<typename T>
+inline void BinarySearchTree<T>::Remove(T value)
+{
+}
+
+template<typename T>
+inline size_t BinarySearchTree<T>::GetSize() const
+{
+	return m_size;
+}
--- bst/Source.cpp
+++ bst/Source.cpp
@@ -2,3 +2,9 @@ int main()
 {
 	return 0;
 }
+
+/*
+	zad dom
+	-szablony cpp (powiedzieæ dlaczego w .h s¹ funkcje a nie w cpp)
+	- search
+*/
