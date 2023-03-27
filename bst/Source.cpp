d0c4e5af5549b5f65dbb1d63df3fe473e8e577b1
Mateusz Talma <talmamateusz@gmail.com>
2023-03-27 19:39:03 +0200

    Added validation for memory leaks

--- bst/Source.cpp
+++ bst/Source.cpp
@@ -1,8 +1,17 @@
 #include "BinarySearchTree.h"
 #include <iostream>
+#include "windows.h"
+#define _CRTDBG_MAP_ALLOC //to get more details
+#include <stdlib.h>  
+#include <crtdbg.h>   //for malloc and free
 
 int main()
 {
+	_CrtMemState sOld;
+	_CrtMemState sNew;
+	_CrtMemState sDiff;
+	_CrtMemCheckpoint(&sOld); //take a snapshot
+
 	BinarySearchTree<int> bst;
 	bst.Insert(90);
 	bst.Insert(80);
@@ -20,4 +29,15 @@ int main()
 	bst.Remove(80);
 	std::cout << '\n';
 	bst.PrintAll();
+
+	_CrtMemCheckpoint(&sNew); //take a snapshot 
+	if (_CrtMemDifference(&sDiff, &sOld, &sNew)) // if there is a difference
+	{
+		OutputDebugString(L"-----------_CrtMemDumpStatistics ---------");
+		_CrtMemDumpStatistics(&sDiff);
+		OutputDebugString(L"-----------_CrtMemDumpAllObjectsSince ---------");
+		_CrtMemDumpAllObjectsSince(&sOld);
+		OutputDebugString(L"-----------_CrtDumpMemoryLeaks ---------");
+		_CrtDumpMemoryLeaks();
+	}
 }
